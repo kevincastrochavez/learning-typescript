95dff5ebc21762575b972566ee81c4f0c8b258fd
Kevin Castro <kevin.castro.chavez0@gmail.com>
2022-04-19 10:26:35 -0600

    learned about types in arrays, objects and arrays of objects

--- basics.ts
+++ basics.ts
@@ -6,3 +6,24 @@ userName = 'Kevin';
 
 let isStudent: boolean;
 isStudent = true;
+
+// Types in arrays
+let hobbies: string[];
+hobbies = ['Sports', 'Cooking'];
+
+// Define the type of an object
+let person: {
+  name: string;
+  age: number;
+};
+
+person = {
+  name: 'Kevin',
+  age: 25,
+};
+
+// Array of objects
+let people: {
+  name: string;
+  age: number;
+}[];
